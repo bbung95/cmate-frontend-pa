f859f340f9422c8f01869f2d805cd6dbf6865c69
bbung <tjd123123@naver.com>
2026-03-09 15:06:09 +0900

    feat: eslint & prettier add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,48 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	// Override default ignores of eslint-config-next.
+	...nextVitals,
+	...nextTs,
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
+	]),
+	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+			'react/no-unknown-property': ['error', { ignore: ['css'] }],
+			'react/react-in-jsx-scope': 'off',
+			'react/no-unescaped-entities': 'off',
+			'react/prop-types': 'off',
+			'jsx-a11y/label-has-associated-control': 'off',
+			'jsx-a11y/accessible-emoji': 'off',
+			'jsx-a11y/click-events-have-key-events': 'off',
+			'jsx-a11y/no-static-element-interactions': 'off',
+			'jsx-a11y/no-noninteractive-element-interactions': 'off',
+			'jsx-a11y/anchor-is-valid': [
+				'error',
+				{
+					components: ['Link'],
+					specialLink: ['hrefLeft', 'hrefRight'],
+					aspects: ['invalidHref', 'preferButton'],
+				},
+			],
+		},
+	},
 ]);
 
 export default eslintConfig;
